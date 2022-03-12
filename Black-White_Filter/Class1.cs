459c145880f150ec49fbf8a0a4bc5202068216cb
Никита Копылов <nekopylov@edu.hse.ru>
2022-03-12 18:23:24 +0500

    Добавьте файлы проекта.

---
+++ Black-White_Filter/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using PluginInterface;
+
+namespace Black_White_Filter
+{
+    [Version(1, 0)]
+    public class GrayFilters : IPlugin
+    {
+        public string Name
+        {
+            get
+            {
+                return "Черно-белый";
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return "Me";
+            }
+        }
+
+        public void Transform(Bitmap bitmap)
+        {
+            for (int j = 0; j < bitmap.Height; j++)
+                for (int i = 0; i < bitmap.Width; i++)
+                {
+                   
+                    UInt32 pixel = (UInt32)(bitmap.GetPixel(i, j).ToArgb());
+                    float R = (float)((pixel & 0x00FF0000) >> 16); 
+                    float G = (float)((pixel & 0x0000FF00) >> 8); 
+                    float B = (float)(pixel & 0x000000FF); 
+                    R = G = B = (R + G + B) / 3.0f;
+                    UInt32 newPixel = 0xFF000000 | ((UInt32)R << 16) | ((UInt32)G << 8) | ((UInt32)B);
+                    bitmap.SetPixel(i, j, Color.FromArgb((int)newPixel));
+                }
+        }
+
+    }
+}
---
+++ MainApp/Form1.cs
@@ -0,0 +1,86 @@
+﻿using PluginInterface;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace MainApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            FindPlugins();
+            CreatePluginsMenu();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        void FindPlugins()
+        {
+            // папка с плагинами
+            string folder = System.AppDomain.CurrentDomain.BaseDirectory;
+
+            // dll-файлы в этой папке
+            string[] files = Directory.GetFiles(folder, "*.dll");
+
+            foreach (string file in files)
+                try
+                {
+                    Assembly assembly = Assembly.LoadFile(file);
+
+                    foreach (Type type in assembly.GetTypes())
+                    {
+                        Type iface = type.GetInterface("PluginInterface.IPlugin");
+
+                        if (iface != null)
+                        {
+                            IPlugin plugin = (IPlugin)Activator.CreateInstance(type);
+                            plugins.Add(plugin.Name, plugin);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка загрузки плагина\n" + ex.Message);
+                }
+            }
+        private void OnPluginClick(object sender, EventArgs args)
+        {
+            IPlugin plugin = plugins[((ToolStripMenuItem)sender).Text];
+            plugin.Transform((Bitmap)pictureBox1.Image);
+            pictureBox1.Refresh();
+        }
+        Dictionary<string, IPlugin> plugins = new Dictionary<string, IPlugin>();
+        //public PictureForm()
+        //{
+        //    InitializeComponent();
+        //    FindPlugins();
+        //    CreatePluginsMenu();
+        //}
+
+        void CreatePluginsMenu()
+        {
+            foreach (IPlugin p in plugins.Values)
+            {
+                var menuItem = new ToolStripMenuItem(p.Name);
+                menuItem.Click += OnPluginClick;
+
+                filtersToolStripMenuItem.DropDownItems.Add(menuItem);
+            }
+
+        }
+
+        private void filtersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CreatePluginsMenu();
+        }
+
+        
+    }
+}
